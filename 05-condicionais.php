47879a5a448903266ba44536fed995b53eafaebc
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-04 11:42:32 -0300

    Adiciona condicional SIMPLES

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -9,11 +9,22 @@
     <h1>Estruturas condicionais</h1>
     <hr>
 
-    <h2>Simples</h2>    
+    <h2>Simples usando <code>if</code></h2>    
+<?php
+$numero = 5;
+
+if($numero > 1){
+    echo "<p>$numero é maior que 1</p>";
+}
+
+// Sintaxe omitindo as chaves
+if($numero > 1) echo "<p>$numero é maior que 1</p>";
+?>
+
 
     <h2>Composta</h2>
 
     <h2>Encadeada</h2>
-    
+
 </body>
 </html>
