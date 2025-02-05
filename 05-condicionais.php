894994b75f8bbc1bfcfa33100de13a05e8898474
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-05 08:34:53 -0300

    Adiciona exemplo condicional composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -20,10 +20,24 @@ if($numero > 1){
 // Sintaxe omitindo as chaves
 if($numero > 1) echo "<p>$numero é maior que 1</p>";
 ?>
-
-
+    <hr>
     <h2>Composta</h2>
+<?php
+$produto = "Ultrabook Asus";
+$qtdEmEstoque = 1; // o que temos
+$qtdCritica = 5; // mínimo necessário
 
+echo "<h3>$produto</h3>";
+echo "<h4>Quantidade em estoque: $qtdEmEstoque</h4>";
+
+if ($qtdEmEstoque < $qtdCritica) {
+    echo "<p>É necessário comprar!</p>";
+} else {
+    echo "<p>Estoque normal</p>";
+}
+
+?>
+    <hr>
     <h2>Encadeada</h2>
 
 </body>
