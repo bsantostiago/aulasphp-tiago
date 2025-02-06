d642d6402dcbb6257fc88e2cd72a831ded938169
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-06 08:24:46 -0300

    Cria arquivo 06 operadores lógicos

---
+++ 06-operadores-logicos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Operadores Lógicos</title>
+</head>
+<body>
+    <h1>Relembrando Operadores Lógicos</h1>
+    <hr>
+
+    <h2>&& (E/AND)</h2>
+    
+    
+    <!-- o símbolo | é chamado de pipe -->
+    <h2>|| (OU/OR)</h2>
+    
+    
+    <h2>! (NÃO/NOT)</h2>
+    
+
+</body>
+</html>
