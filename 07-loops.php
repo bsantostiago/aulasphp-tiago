c88cce15484f34f8190bc45c00a3a3c41997bd07
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-06 11:17:46 -0300

    Adiciona foreach com array associativo

--- 07-loops.php
+++ 07-loops.php
@@ -96,6 +96,21 @@ foreach( $alunos as $aluno ) {
 }
 ?>
 
+    <h3>Acessando array associativo</h3>
+<?php
+$clubes = [
+    "Corinthians" => "Timão",
+    "Palmeiras" => "Porco",
+    "São Paulo" => "Tricolor",
+    "Santos" => "Peixe"
+];
+
+foreach($clubes as $clube => $apelido) {
+?>
+    <p>O clube <?=$clube?> é conhecido como <?=$apelido?></p>
+<?php
+}
+?>
 
 
 </body>
