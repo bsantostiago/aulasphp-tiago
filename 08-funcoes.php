9d809584ccacddac063b5b91b8666652f441335c
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-11 09:02:12 -0300

    Adiciona função com parâmetros e retorno

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -36,7 +36,50 @@ function exibirDadosDoAutor(){
         </ul>
 
         <hr>
+
+        <h2>Função com parâmetros (ou argumentos)</h2>
+<?php
+function somar($valor1, $valor2){
+    /* Variável de escopo LOCAL:
+    Este tipo de variável existe SOMENTE DENTRO da função em que foi declarada. */
+    //$total = $valor1 + $valor2;
+
+    /* Retornamos o resultado, ou seja, "mandamos para fora" da função
+    o resultado do que ela fez. */
+    //return $total;
+
+    // Podemos também aplicar o return direto na expressão (sem uso de variável local)
+    return $valor1 + $valor2;
+}
+?>
+        <h3>Chamadas/retornos da função somar</h3>
+        <p>Resultado 1: <?=somar(10, 5)?> </p>
+        <p>Resultado 2: <?=somar(23, 107)?> </p>
+
+<?php
+// Chamando a função e GUARDANDO o resultado numa variável GLOBAL
+$resultado3 = somar(1250, 788.85);
+?>
+        <p>Resultado 3: <?=$resultado3?></p>
+
+<?php if($resultado3 > 2000){ ?>
+        <p class="alert alert-success">O resultado 3 é maior que 2000</p>
+<?php } else { ?>
+        <p class="alert alert-danger">Resultado 3 não é maior que 2000</p>
+<?php } ?>
+
+        <!-- Usando uma função como parte de uma condição:
+         PRIMEIRO, a função é chamada (e aí ela calcula e retorna), 
+         DEPOIS o resultado que ela retornou é comparado com a condição -->
+<?php if( somar(10, 50) < 500 ){ ?>       
+        <p>João Pedro reprovado...</p>
+<?php } ?>
+
+        
+
     </div>    
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
