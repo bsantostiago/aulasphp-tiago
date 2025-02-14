6b3447d54087e402763c7be67daf8b9161717986
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-14 09:04:13 -0300

    Reorganiza includes

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,4 +1,4 @@
-<?php include "cabecalho.php"; ?>
+<?php include "includes/cabecalho.php"; ?>
 
             <h2>Página de dúvidas</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis corporis temporibus, nemo sit voluptatum quam natus unde amet perferendis similique possimus id quia asperiores ipsa, nobis voluptates rerum quidem aperiam.</p>
@@ -12,13 +12,5 @@
                 <summary>Os cursos são gratuitos?</summary>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis explicabo delectus recusandae cumque eaque vero a nihil vel quae?</p>
             </details>
-        </main>
-        
-        <footer>
-            <p>Site criado para o curso Téc. Informática para Internet</p>
-        </footer>
-    </div>    
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include "includes/rodape.php"; ?>        
