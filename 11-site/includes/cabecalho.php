f99809c2241626c700bd0555f00d88aaa5ae3411
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-14 11:06:42 -0300

    Adiciona titulo personalizado

--- 11-site/includes/cabecalho.php
+++ 11-site/includes/cabecalho.php
@@ -1,10 +1,32 @@
+<?php
+// Capturamos o nome do arquivo atualmente aberto
+$pagina = basename($_SERVER["PHP_SELF"]);
+
+// Analisamos o valor de $pagina pra determinar um título personalizado
+switch ($pagina) {
+    case 'index.php':
+        $titulo = "Página Inicial";
+        break;
+    case 'cursos.php':
+        $titulo = "Treinamentos";
+        break;
+    case 'duvidas.php':
+        $titulo = "Dúvidas";
+        break;    
+    default:
+        $titulo = "Contato";
+        break;
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com PHP</title>
+
+    <title> <?=$titulo?> - Site com PHP</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
