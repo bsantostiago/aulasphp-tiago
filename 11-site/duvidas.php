c873cbae3e105538ab51181667cf942079650cba
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-02-14 08:36:57 -0300

    Cria página dúvidas

---
+++ 11-site/duvidas.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dúvidas - Site com PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Dúvidas</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Página de dúvidas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis corporis temporibus, nemo sit voluptatum quam natus unde amet perferendis similique possimus id quia asperiores ipsa, nobis voluptates rerum quidem aperiam.</p>
+
+            <h3>Principais dúvidas</h3>
+            <details>
+                <summary>Como se cadastrar?</summary>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, ad!</p>
+            </details>
+            <details>
+                <summary>Os cursos são gratuitos?</summary>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis explicabo delectus recusandae cumque eaque vero a nihil vel quae?</p>
+            </details>
+        </main>
+        
+        <footer>
+            <p>Site criado para o curso Téc. Informática para Internet</p>
+        </footer>
+    </div>    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
